09cd65fb0899d1a21693360b7fbbe0fa27dbf7ae
sabrina <sabkias@hotmail.com>
2022-01-14 21:55:22 -0500

    correction

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 
-/*function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}*/
