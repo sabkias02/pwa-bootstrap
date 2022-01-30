db68c474bfd208c4b0a7927fbe8feca84e12881b
sabrina <sabkias@hotmail.com>
2022-01-30 10:35:47 -0500

    icons added

--- sw.js
+++ sw.js
@@ -74,6 +74,8 @@ self.addEventListener('activate', function(event) {
       caches.open('v1').then(function(cache) {
         return cache.addAll([
            '/',
+           '/index.html',
+
         ]);
       })
     );
