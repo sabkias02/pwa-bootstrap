78e1f1b010088f05945ccd2ea3a486917c6bfef7
sabrina <sabkias@hotmail.com>
2022-01-30 21:46:22 -0500

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js');
+    navigator.serviceWorker.register('/pwa-bootstrap/sw.js');
     console.log('registered successfully')
  };
  
