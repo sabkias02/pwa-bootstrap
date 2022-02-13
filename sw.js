c3b8ab02d6150acac92190123396802b7bcc7ef3
sabrina <sabkias@hotmail.com>
2022-02-13 12:11:14 -0500

    page2 et hors ligne

--- sw.js
+++ sw.js
@@ -14,6 +14,7 @@
            '/pwa-bootstrap/SheratonAnnabaHotel.png',
            '/pwa-bootstrap/bootstrap-5.1.3-dist/js/bootstrap.min.js',
            '/pwa-bootstrap/bootstrap-5.1.3-dist/css/bootstrap.min.css',
+           '/pwa-bootstrap/pageHorsLigne.html',
            /* '/pwa-bootstrap/icons-1.7.2/font/fonts/bootstrap.icons.woff',
            '/pwa-bootstrap/icons-1.7.2/font/fonts/bootstrap.icons.woff2',
            '/pwa-bootstrap/icons-1.7.2/font/bootstrap.icons.css', */
@@ -23,7 +24,7 @@
       })
     );
   });
-  this.addEventListener('fetch', function(event) {
+  /*this.addEventListener('fetch', function(event) {
     console.log("Fetching ..." + event.request.url);
     event.respondWith(caches.match(event.request).then((response) => {
         if (response !== undefined) {
@@ -33,7 +34,37 @@
             return fetch(event.request);
         }
     }))
- })
+ })*/
+ /* function enregistrerTagBgSync() {
+ if ('serviceWorker' in navigator && 'SyncManager' in window) {
+     navigator.serviceWorker.ready.then(function (reg) {
+         return reg.sync.register('mon-tag');
+     });
+ };
+}*/ 
+this.addEventListener('fetch', function(event) {
+ console.log("Fetching ..." + event.request.url);
+ event.respondWith(cacheOrNetwork(event.request).catch(() => fallbackVersPageHorsLigne()));
+});
+
+
+function cacheOrNetwork(request) {
+return fromCache(request).catch(() => fetch(request));
+};
+
+function fromCache(request) {
+return caches.open('v1').then(function (cache) {
+ return cache.match(request).then(function (matching) {
+   return matching || Promise.reject('no-match');
+ });
+ 
+});
+}
+
+function fallbackVersPageHorsLigne() {
+return caches.match('/pageHorsLigne.html');
+} 
+
 
 
 
